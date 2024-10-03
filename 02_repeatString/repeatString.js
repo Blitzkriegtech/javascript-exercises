8f21ff8afee003822182b0760e7cdd24c50d363a
Blitzkriegtech <aljunrendon@yahoo.com>
2024-10-03 22:10:26 +0800

    Use conditional and for loops to beat the challenge

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    //  if (num < 0) return "";
+    //  if (num === 1) return str;
+    //  else 
+    //      return str + repeatString(str, num -1);
+    let rpStr = "";
+    for (i = 0; i < num; i++) {
+         rpStr += str;
+    }
+       return rpStr;
 };
 
 // Do not edit below this line
