fb150300e004a5c0f4fea8531394a1c35e80eb27
Blitzkriegtech <aljunrendon@yahoo.com>
2024-10-04 20:39:56 +0800

    Polish solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,23 @@
 const repeatString = function(str, num) {
-    //  if (num < 0) return "";
+
+    //* Solution 1
+    //  if (num === 0) return "";
+    //  if (num < 0) return "ERROR";
     //  if (num === 1) return str;
     //  else 
     //      return str + repeatString(str, num -1);
+
+    //* Solution 2s
+    if (num < 0) 
+        return "ERROR";
+    
     let rpStr = "";
     for (i = 0; i < num; i++) {
-         rpStr += str;
+        rpStr += str;
     }
-       return rpStr;
+    return rpStr;
+    
+       
 };
 
 // Do not edit below this line
