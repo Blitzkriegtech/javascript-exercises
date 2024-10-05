6c8b95c5e1bcc945848b731f9e1677183e5632a5
Blitzkriegtech <aljunrendon@yahoo.com>
2024-10-05 16:06:12 +0800

    Add two solution for the challenge 1. .filter() method 2. Combination of for loop and .splice() method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+//  *Solution 1
+// return array.filter(val != args.includes(val));
+
+// *Solution 2
+    for (let i = 0; i < array.length; i++)
+    {
+        if (args.includes(array[i])) 
+            {
+                array.splice(i, 1);
+                i--;
+            }
+    }
+    return array;
 };
 
 // Do not edit below this line
